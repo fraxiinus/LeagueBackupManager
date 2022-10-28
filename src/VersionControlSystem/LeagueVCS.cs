920bab7f96b8ed0827f8ba067f6f1094c7fb52e0
Fraxiinus <8365233+fraxiinus@users.noreply.github.com>
2022-10-27 21:56:44 -0400

    updates

---
+++ src/VersionControlSystem/LeagueVCS.cs
@@ -0,0 +1,14 @@
+﻿namespace LeagueBackupManager.VersionControlSystem;
+using System.Text.RegularExpressions;
+
+public class LeagueVCS
+{
+    private const string DB_NAME = "vcs.json";
+    private readonly string _repositoryPath;
+    private List<Regex> _filters = new();
+
+    public LeagueVCS(string repoPath)
+    {
+        _repositoryPath = repoPath;
+    }
+}
---
+++ src/VersionControlSystem/NativeMethods.cs
@@ -0,0 +1,8 @@
+﻿namespace LeagueBackupManager.VersionControlSystem;
+using System.Runtime.InteropServices;
+
+internal static class NativeMethods
+{
+    [DllImport("Kernel32", CharSet = CharSet.Unicode)]
+    internal static extern bool CreateHardLink(string linkName, string sourceName, IntPtr attribute);
+}
